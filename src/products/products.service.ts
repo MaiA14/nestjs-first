5c340fdd55b0da09339218da795a7ea7e8c4e903
Mai <mai@Drors-MacBook-Pro-2.local>
2024-07-04 18:44:56 +0300

    use uuid

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,12 +1,13 @@
 import { Injectable, NotFoundException } from "@nestjs/common";
 import { Product } from "./product.model";
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class ProductsService {
     products: Product[] = [];
 
     insertProduct(title: string, description: string, price: number) {
-        const prodId = Math.random().toString();
+        const prodId = uuidv4();
         const newProduct = new Product(prodId, title, description, price);
         this.products.push(newProduct);
         return prodId;
